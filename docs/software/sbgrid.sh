e9184e62da710d2f478817c8ee3584ede93e75af
hb <hb@aroma-project.org>
2021-01-14 08:47:14 -0800

    MDI: singularity.sh cleans up user and group names + disable user-specific startup for Rscript example

--- docs/software/sbgrid.sh
+++ docs/software/sbgrid.sh
@@ -1,12 +1,19 @@
 #! /usr/bin/env bash
 
-MDI_USER='alice'
-MDI_HOSTNAME='{{ site.devel.name }}'
-PS1="[\u@\h \W]\$ "
-
 ## Sanity checks
 groups | grep -qw "sbgrid" || { 2>&1 echo "ERROR: User is not in the 'sbgrid' group: $USER"; exit 1; }
 [[ -f /programs/sbgrid.shrc ]] || { 2>&1 echo "ERROR: No access to /programs/sbgrid.shrc"; exit 1; }
+
+# shellcheck disable=SC2034
+MDI_USER='alice"
+MDI_HOSTNAME="{{ site.devel.name }}"
+PS1="[\u@\h \W]\$ "
+
+mdi_adjust_output() {
+    local tilde
+    tilde='~'
+    mdi_replace_pwd | sed "s|${HOME}|${tilde}|g" | sed "s|${TMPDIR}|${tilde}|g" 
+}
 
 mdi_code_block --label="groups" <<EOF
 groups
--- docs/software/singularity.sh
+++ docs/software/singularity.sh
@@ -1,8 +1,17 @@
 #! /usr/bin/env bash
 
-MDI_USER='alice'
-MDI_HOSTNAME='{{ site.devel.name }}'
+# shellcheck disable=SC2034
+MDI_USER="alice"
+MDI_GROUP="boblab"
+MDI_HOSTNAME="{{ site.devel.name }}"
 PS1="[\u@\h \W]\$ "
+
+mdi_adjust_output() {
+    local tilde
+    tilde='~'
+    GROUP=$(id --name --group)
+    mdi_replace_pwd | sed "s|${HOME}|${tilde}|g" | sed "s|${TMPDIR}|${tilde}|g" | sed "s|\b${USER}\b|${MDI_USER}|g" | sed "s|\b${GROUP}\b|${MDI_GROUP}|g"
+}
 
 mdi_code_block --label="build" <<EOF
 mkdir lxc
@@ -12,6 +21,8 @@ ls -l rocker_r-base.img
 EOF
 
 
+export R_PROFILE_USER=""
+export R_ENVIRON_USER=""
 mdi_code_block --label="rscript-sum" --workdir=lxc <<EOF
 singularity exec rocker_r-base.img Rscript -e "sum(1:10)"
 
