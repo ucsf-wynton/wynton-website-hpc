39bfe8b8eb6ed0909a54e98def353b5f2941734a
hb <hb@aroma-project.org>
2021-01-14 08:28:43 -0800

    MDI: sbgrid.sh asserts that it's run by user in 'sbgrid'

--- docs/software/sbgrid.sh
+++ docs/software/sbgrid.sh
@@ -4,6 +4,10 @@ MDI_USER='alice'
 MDI_HOSTNAME='{{ site.devel.name }}'
 PS1="[\u@\h \W]\$ "
 
+## Sanity checks
+groups | grep -qw "sbgrid" || { 2>&1 echo "ERROR: User is not in the 'sbgrid' group: $USER"; exit 1; }
+[[ -f /programs/sbgrid.shrc ]] || { 2>&1 echo "ERROR: No access to /programs/sbgrid.shrc"; exit 1; }
+
 mdi_code_block --label="groups" <<EOF
 groups
 EOF
