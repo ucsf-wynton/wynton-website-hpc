30707bde075dfec23ec91505df3d54cf0b78f681
hb <hb@aroma-project.org>
2021-05-11 16:53:06 -0700

    ROBUSTNESS: Run R examples in vanilla mode

--- docs/software/singularity.sh
+++ docs/software/singularity.sh
@@ -3,6 +3,8 @@
 
 export R_PROFILE=
 export R_ENVIRON=
+export R_ENVIRON_USER=
+export R_PROFILE_USER=
 
 MDI_USER='alice'
 MDI_GROUP='boblab'
@@ -16,15 +18,15 @@ if [[ ! -d "lxc" ]]; then
 mdi_code_block --label="build" <<EOF
 mkdir lxc
 cd lxc/
-singularity build rocker_r-base.img docker://rocker/r-base
-ls -l rocker_r-base.img
+singularity build rocker_r-base.sif docker://rocker/r-base
+ls -l rocker_r-base.sif
 EOF
 fi
 
 1>&2 echo "WARNING: .mdi/singularity.sh.code-block.label=run needs to be updated manually"
 
 mdi_code_block --label="rscript-sum" --workdir=lxc <<EOF
-singularity exec rocker_r-base.img Rscript -e "sum(1:10)"
+singularity exec rocker_r-base.sif Rscript -e "sum(1:10)"
 
 EOF
 
