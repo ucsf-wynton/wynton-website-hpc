ab0e366c68c3856cd6db59f08b49342a0c88c018
Henrik Bengtsson <hb@aroma-project.org>
2021-07-14 23:58:59 +0200

    ShellCheck: fix two mistakes

--- cron-scripts/wynton-users.sh
+++ cron-scripts/wynton-users.sh
@@ -3,14 +3,14 @@
 SOFTWARE_HOME=/wynton/home/cbi/shared/software/CBI
 PATH=/opt/sge/bin/lx-amd64/:$PATH
 PATH=${SOFTWARE_HOME}/R-3.6.1/bin:$PATH
-R_LIBS_USER=${HOME}/R/x86_64-pc-linux-gnu-library/3.6-CBI 
+export R_LIBS_USER=${HOME}/R/x86_64-pc-linux-gnu-library/3.6-CBI 
 
 WORKDIR=${WORKDIR:-$HOME/repositories/UCSF-HPC/wynton/docs}
 
 date --rfc-3339=seconds
 
 (
-    cd "$WORKDIR"; 
+    cd "$WORKDIR" || exit 1;
     make _data/users.yml;
     make deploy;
 )
