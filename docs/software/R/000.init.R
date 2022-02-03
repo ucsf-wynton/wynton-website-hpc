1b872f58ee4b5c87ca5c4b8100dbc855a4c53166
Henrik Bengtsson <hb@aroma-project.org>
2022-02-03 00:11:34 -0800

    R: Add parse_module()

--- docs/software/R/000.init.R
+++ docs/software/R/000.init.R
@@ -100,3 +100,65 @@ get_modules_sets <- function(module_repositories) {
 
   modules_sets
 }
+
+
+parse_module <- function(m) {
+  m <- lapply(m, FUN = trim)
+
+  ## Trim versions
+  versions <- m$versions[[1]]
+  for (cc in seq_len(ncol(versions))) {
+    versions[[cc]] <- trim(versions[[cc]])
+  }
+  m$versions[[1]] <- versions
+
+  ## Identify parents
+  parent <- m$versions[[1]]$parent
+  parent <- unique(unlist(parent, use.names = FALSE))
+  ## FIXME /HB 2021-05-05
+  parent <- setdiff(parent, c("built-in", "Sali"))
+  parent <- setdiff(parent, c("CBI", "Sali"))
+  if (length(parent) > 0 && !is.element(parent, c("CBI-testing")))
+    parent <- character(0)
+  stopifnot(length(parent) <= 1L)
+  m$parent <- parent
+
+  ## Parse help
+  helps <- unique(versions$help)
+  help <- helps[[1]]
+  vers <- NULL
+  if (!is.null(versions$versionName)) {
+    vers <- unique(versions$versionName)
+    if (length(vers) > 1L || vers != "default") {
+      idx <- match(m$defaultVersionName, table = vers)
+      if (length(idx) == 1) {
+        ## FIXME: spider seems to set 'defaultVersionName' randomly /HB 2017-06-30
+        vers[idx] <- sprintf("<em>%s</em>", vers[idx])
+      }
+    } else {
+      vers <- NULL
+    }
+  }
+  m$vers <- vers
+  
+  ## Extract 'Nnnn: ' content from the end.
+  if (length(m$description) > 0L) {
+    pattern <- "(.*)[[:space:]]+(Examples?|Note|Warning|Maintainer):[[:space:]]+(.*)"
+    while(grepl(pattern, m$description)) {
+      name <- gsub(pattern, "\\2", m$description)
+      name <- sub("s$", "", name)
+      name <- tolower(name)
+      m[[name]] <- gsub(pattern, "\\3", m$description)
+      m$description <- gsub(pattern, "\\1", m$description)
+    }
+  }
+  
+  ## Trim fields
+  for (field in c("description", "url", "warning")) {
+    if (is.null(m[[field]])) next
+    if (is.na(m[[field]])) m[[field]] <- NULL
+  }
+
+  m
+} ## parse_module()
+
