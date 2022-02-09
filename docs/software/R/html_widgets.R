0c5adb7d8117d1803987beaafbfb746843706702
Henrik Bengtsson <hb@aroma-project.org>
2022-02-08 22:12:14 -0800

    Keep capitalization

--- docs/software/R/html_widgets.R
+++ docs/software/R/html_widgets.R
@@ -4,7 +4,7 @@ repos_buttons <- function(modules_sets) {
     name <- names(modules_sets)[kk]
     modules <- modules_sets[[name]]
     nbr_of_modules <- nrow(modules)
-    cat('<li', if (kk == 1) ' class="active"', '><a data-toggle="pill" href="#queues-', tolower(name), '"><span style="font-weight: bold;">', tolower(name), '</span>&nbsp;(', nbr_of_modules, ')</a></li>\n', sep = "")
+    cat('<li', if (kk == 1) ' class="active"', '><a data-toggle="pill" href="#queues-', tolower(name), '"><span style="font-weight: bold;">', name, '</span>&nbsp;(', nbr_of_modules, ')</a></li>\n', sep = "")
   }
   cat('</ul>\n')
 }
