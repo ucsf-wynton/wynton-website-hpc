98fbb5c713b7f69f7bf40911a799da5cbd4a40cd
Henrik Bengtsson <hb@aroma-project.org>
2022-02-08 21:40:37 -0800

    REPOS: Be agile to hash #module-software-repository-<repo> in URLs

--- docs/software/R/html_widgets.R
+++ docs/software/R/html_widgets.R
@@ -1,4 +1,4 @@
-repos_buttons <- function(modules_sets, where = c("before", "after")) {
+repos_buttons <- function(modules_sets) {
   where <- match.arg(where)
   
   cat('<ul class="nav nav-pills">\n')
@@ -6,7 +6,7 @@ repos_buttons <- function(modules_sets, where = c("before", "after")) {
     name <- names(modules_sets)[kk]
     modules <- modules_sets[[name]]
     nbr_of_modules <- nrow(modules)
-    cat('<li id="nav-pill-', where, '-', name, '"', if (kk == 1) ' class="active"', '><a data-toggle="pill" href="#queues-', name, '"><span style="font-weight: bold;">', name, '</span>&nbsp;(', nbr_of_modules, ')</a></li>\n', sep = "")
+    cat('<li', if (kk == 1) ' class="active"', '><a data-toggle="pill" href="#queues-', name, '"><span style="font-weight: bold;">', tolower(name), '</span>&nbsp;(', nbr_of_modules, ')</a></li>\n', sep = "")
   }
   cat('</ul>\n')
 }
