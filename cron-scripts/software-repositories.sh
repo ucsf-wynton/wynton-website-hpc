68b84cce9c2b15c2b6a971312d41cce090ce2e7b
hb <hb@aroma-project.org>
2022-07-27 23:05:43 -0700

    Website updated [ci skip]

--- cron-scripts/software-repositories.sh
+++ cron-scripts/software-repositories.sh
@@ -8,11 +8,11 @@ echo "Wynton HPC website: Update Software Repository page ..."
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ## Required software
-PATH="/wynton/home/cbi/shared/software/CBI/R-3.6.1/bin:$PATH"
+PATH="/wynton/home/cbi/shared/software/CBI/R-4.2.1/bin:$PATH"
 
 ## Required R packages
 ## FIXME: Freeze an R package library with the required packages
-export R_LIBS_USER=/wynton/home/cbi/hb/R/x86_64-pc-linux-gnu-library/3.6-CBI
+export R_LIBS_USER=/wynton/home/cbi/hb/R/x86_64-pc-linux-gnu-library/4.2-CBI-gcc10
 
 ## Session info
 Rscript --version
@@ -20,6 +20,7 @@ Rscript -e ".libPaths()"
 
 cd "$SCRIPT_PATH/../docs/software"
 git pull -X theirs
+make clean
 make build
 make deploy
 
