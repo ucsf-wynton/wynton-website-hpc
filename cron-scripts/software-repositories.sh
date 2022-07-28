34595f53b807450bf744cc5895b0efdf64c9d88f
hb <hb@aroma-project.org>
2022-07-27 23:11:50 -0700

    Update crontab script to use R 4.2.1 (was R 3.6.1)

--- cron-scripts/software-repositories.sh
+++ cron-scripts/software-repositories.sh
@@ -8,11 +8,11 @@ echo "Wynton HPC website: Update Software Repository page ..."
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ## Required software
-PATH="/wynton/home/cbi/shared/software/CBI/R-3.6.1/bin:$PATH"
+PATH="/wynton/home/cbi/shared/software/CBI/R-4.2.1-gcc10/bin:$PATH"
 
 ## Required R packages
 ## FIXME: Freeze an R package library with the required packages
-export R_LIBS_USER=/wynton/home/cbi/hb/R/x86_64-pc-linux-gnu-library/3.6-CBI
+export R_LIBS_USER=/wynton/home/cbi/hb/R/x86_64-pc-linux-gnu-library/4.2-CBI-gcc10
 
 ## Session info
 Rscript --version
--- cron-scripts/wynton-users.sh
+++ cron-scripts/wynton-users.sh
@@ -2,8 +2,8 @@
 
 SOFTWARE_HOME=/wynton/home/cbi/shared/software/CBI
 PATH=/opt/sge/bin/lx-amd64/:$PATH
-PATH=${SOFTWARE_HOME}/R-3.6.1/bin:$PATH
-export R_LIBS_USER=${HOME}/R/x86_64-pc-linux-gnu-library/3.6-CBI 
+PATH=${SOFTWARE_HOME}/R-4.2.1-gcc10/bin:$PATH
+export R_LIBS_USER=${HOME}/R/x86_64-pc-linux-gnu-library/4.2-CBI-gcc10
 
 WORKDIR=${WORKDIR:-$HOME/repositories/UCSF-HPC/wynton/docs}
 
