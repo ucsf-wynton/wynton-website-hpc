26fa64ab622cbe6b927e2f1941e76389d6eef1ca
hb <hb@aroma-project.org>
2022-11-03 17:50:40 -0700

    Forgot to add 'lmodweb.config.R'

---
+++ docs/hpc/software/lmodweb.config.R
@@ -0,0 +1,29 @@
+#' @export
+module_repositories <- list(
+  "built-in" = list(
+    name        = "built-in",
+    maintainer  = "{{ site.cluster.nickname }} Systems Administrators",
+    group       = "{{ site.cluster.name }}",
+    url         = "{{ '/about/contact.html' | relative_url }}",
+    contributed = FALSE,
+    module_path = c("/wynton/home/opt/modules", "/etc/modulefiles")[2]
+  ),
+
+  CBI = list(
+    name        = "CBI",
+    maintainer  = "Henrik Bengtsson",
+    group       = "Computational Biology and Informatics",
+    url         = "https://cbi.ucsf.edu",
+    contributed = TRUE,
+    module_path = Sys.getenv("MODULE_ROOT_CBI")
+  ),
+
+  Sali = list(
+    name        = "Sali",
+    maintainer  = "Ben Webb",
+    group       = "Sali Lab Software Repository",
+    url         = "https://salilab.org/",
+    contributed = TRUE,
+    module_path = Sys.getenv("MODULE_ROOT_Sali", "/salilab/diva1/home/modules")
+  )
+)
