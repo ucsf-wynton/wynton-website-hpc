c4102004ce7969cb05eb5fcd00c5c2653b123ebc
hb <hb@aroma-project.org>
2022-12-06 15:10:10 -0800

    Add cron-scripts/wynton-specs.sh [#101]

---
+++ cron-scripts/wynton-specs.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+SOFTWARE_HOME=/wynton/home/cbi/shared/software/CBI
+PATH=/opt/sge/bin/lx-amd64/:$PATH
+PATH=${SOFTWARE_HOME}/R-4.2.1-gcc10/bin:$PATH
+export R_LIBS_USER=${HOME}/R/x86_64-pc-linux-gnu-library/4.2-CBI-gcc10
+
+WORKDIR=${WORKDIR:-$HOME/repositories/UCSF-HPC/wynton/docs}
+
+date --rfc-3339=seconds
+
+(
+    cd "$WORKDIR" || exit 1;
+    make hpc/_data/specs.yml;
+    make deploy;
+)
