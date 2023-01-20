b2eb9a3eba1ed415c7d54207ae49c3bb897b6655
Henrik Bengtsson <hb@aroma-project.org>
2023-01-19 17:09:58 -0800

    BETA: Enable toggle 'View As' Non-PHI/PHI [#51]

--- docs/hpc/assets/js/wynton.js
+++
@@ -1,5 +0,0 @@
-const togglePhi = document.getElementById("toggle-phi");
-togglePhi.checked = (localStorage.getItem("phi-mode") == "true");
-togglePhi.addEventListener("click", (e) => {
-    localStorage.setItem("phi-mode", togglePhi.checked);
-});
