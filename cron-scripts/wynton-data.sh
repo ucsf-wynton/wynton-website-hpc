76c82d01cc6551a2403c53329816f776c6c14cd1
hb <hb@aroma-project.org>
2023-03-22 10:15:51 -0700

    WORKAROUND: 'module' is not available when running cron jobs

--- cron-scripts/wynton-data.sh
+++ cron-scripts/wynton-data.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env bash
 
+## WORKAROUND: 'module' is not available when running cron jobs
 SOFTWARE_HOME=/wynton/home/cbi/shared/software/CBI
 PATH=/opt/sge/bin/lx-amd64/:$PATH
-PATH=${SOFTWARE_HOME}/R-4.2.1-gcc10/bin:$PATH
+PATH=${SOFTWARE_HOME}/R-4.2.2-gcc10/bin:$PATH
+PATH=${SOFTWARE_HOME}/sqlite-3.41.1/bin:$PATH
+
 export R_LIBS_USER=${HOME}/R/x86_64-pc-linux-gnu-library/4.2-CBI-gcc10
 
 WORKDIR=${WORKDIR:-$HOME/repositories/ucsf-wynton/wynton-website-hpc/docs}
