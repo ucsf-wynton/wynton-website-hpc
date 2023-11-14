0da1554e15a2c49ce1dc96ea212bb229f3ccc9ca
hb <hb@aroma-project.org>
2023-11-14 06:57:08 -0800

    Website updated [ci skip]

--- cron-scripts/software-repositories.sh
+++ cron-scripts/software-repositories.sh
@@ -16,7 +16,7 @@ type module
 
 ## Required software
 SOFTWARE_HOME=/wynton/home/cbi/shared/software/CBI
-PATH=${SOFTWARE_HOME}/R-4.3.1-gcc10/bin:$PATH
+PATH=${SOFTWARE_HOME}/_rocky8/R-4.3.2-gcc10/bin:$PATH
 
 ## Required R packages
 ## FIXME: Freeze an R package library with the required packages
--- cron-scripts/wynton-data.sh
+++ cron-scripts/wynton-data.sh
@@ -3,10 +3,13 @@
 ## WORKAROUND: 'module' is not available when running cron jobs
 SOFTWARE_HOME=/wynton/home/cbi/shared/software/CBI
 PATH=/opt/sge/bin/lx-amd64/:$PATH
-PATH=${SOFTWARE_HOME}/R-4.2.2-gcc10/bin:$PATH
+PATH=${SOFTWARE_HOME}/_rocky8/R-4.3.2-gcc10/bin:$PATH
 PATH=${SOFTWARE_HOME}/sqlite-3.41.1/bin:$PATH
 
-export R_LIBS_USER=${HOME}/R/x86_64-pc-linux-gnu-library/4.2-CBI-gcc10
+## Required R packages
+## FIXME: Freeze an R package library with the required packages
+R_LIBS_USER=$(Rscript --vanilla -e "cat(Sys.getenv('R_LIBS_USER'))")
+export R_LIBS_USER
 
 WORKDIR=${WORKDIR:-$HOME/repositories/ucsf-wynton/wynton-website-hpc/docs}
 
