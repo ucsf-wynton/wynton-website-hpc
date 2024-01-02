a4f0668eb6ef2d1f23115de96aa638ff72441680
hb <hb@aroma-project.org>
2024-01-02 07:46:55 -0800

    CRON SCRIPT: Use 'module load CBI r/4.3.2' instead of hardcoding paths

--- cron-scripts/software-repositories.sh
+++ cron-scripts/software-repositories.sh
@@ -14,9 +14,14 @@ SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ## Assert module is defined
 type module
 
+## Load R
+module load CBI
+module load r/4.3.2
+module list
+
 ## Required software
-SOFTWARE_HOME=/wynton/home/cbi/shared/software/CBI
-PATH=${SOFTWARE_HOME}/_rocky8/R-4.3.2-gcc10/bin:$PATH
+#SOFTWARE_HOME=/wynton/home/cbi/shared/software/CBI
+#PATH=${SOFTWARE_HOME}/_rocky8/R-4.3.2-gcc10/bin:$PATH
 
 ## Required R packages
 ## FIXME: Freeze an R package library with the required packages
@@ -24,7 +29,9 @@ R_LIBS_USER=$(Rscript --vanilla -e "cat(Sys.getenv('R_LIBS_USER'))")
 export R_LIBS_USER
 
 ## Session info
+command -v Rscript
 Rscript --version
+echo "R_LIBS_USER=${R_LIBS_USER:-<not set>}"
 Rscript -e ".libPaths()"
 
 cd "$SCRIPT_PATH/../docs/hpc/software"
