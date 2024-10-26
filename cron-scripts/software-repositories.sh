dbf6d9fc4e1d4810a4614e02ac54de6fbcc76bac
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-10-26 12:29:23 -0700

    ShellCheck: Fix SC2015 (info): Note that A && B || C is not if-then-else. C may run when A is true.

--- cron-scripts/software-repositories.sh
+++ cron-scripts/software-repositories.sh
@@ -9,7 +9,9 @@ SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ## Setup shell, including Lmod
 #shellcheck disable=SC1091
-[[ -f /etc/bashrc ]] && . /etc/bashrc || true
+if [[ -f /etc/bashrc ]]; then
+    . /etc/bashrc
+fi
 
 ## Assert module is defined
 type module
